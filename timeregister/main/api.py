e4d68c6d2d299a0dbf3b6450b4806efaba8808ac
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-08-01 14:40:03 -0300

    write tests for the Reports api

--- timeregister/main/api.py
+++ timeregister/main/api.py
@@ -45,27 +45,77 @@ class ReportViewSet(viewsets.ViewSet):
 
     def list(self, request):
         queryset = Report.objects.all()
-        serializer = ReportSerializer(queryset, many=True)
-        return Response(serializer.data)
+        if len(queryset) == 0:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            serializer = ReportSerializer(queryset, many=True)
+            return Response({
+                "data": serializer.data,
+                "message": ""
+            }, status=status.HTTP_200_OK)
     
     def create(self, request):
+        try:
+            Employee.objects.get(id=request.data['employee_id'])
+        except ObjectDoesNotExist:
+            return Response({
+                "data": "",
+                "message": "this employee does not exists"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            TypeContract.objects.get(id=request.data['typeContract_id'])
+        except ObjectDoesNotExist:
+            return Response({
+                "data": "",
+                "message": "this type of contract does not exists"
+            }, status=status.HTTP_400_BAD_REQUEST)
         serializer = ReportSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             new_report = serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "data": serializer.data,
+                "message": "report added"
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            "data": serializer.errors,
+            "message": "error adding report"
+        }, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk):
+        try:
+            Report.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response({
+                "data": "",
+                "message": "no report founded"
+            }, status=status.HTTP_400_BAD_REQUEST)
         serializer = ReportSerializer
         deleted_register = serializer.destroy(serializer, id=pk)
-        return Response("", status=status.HTTP_204_NO_CONTENT)
+        return Response({
+            "data": "",
+            "message": "report deleted"
+        }, status=status.HTTP_204_NO_CONTENT)
 
     def retrieve(self, request, pk):
+        try:
+            Report.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response({
+                "data": "",
+                "message": "no report founded"
+            }, status=status.HTTP_400_BAD_REQUEST)
         report = Report.retrieve(Report, id=pk)
         serializer = ReportRetrieveSerializer(data=report)
         if serializer.is_valid(raise_exception=True):
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+            return Response({
+                "data": serializer.data,
+                "message": ""
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "data": serializer.errors,
+            "message": "error when returning report"
+        }, status=status.HTTP_400_BAD_REQUEST)
+        Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 
 class TimesReportViewSet(viewsets.ViewSet):
---
+++ timeregister/main/tests/tests_reports.py
@@ -0,0 +1,128 @@
+import json
+import unittest
+from datetime import date, time
+
+from django.test import Client, TestCase
+
+from main.models import Employee, Report, TypeContract
+
+
+def add_new_employee():
+    employee_test = Employee(
+        name="Employee Test"
+    )
+    employee_test.save()
+    return employee_test
+
+def add_new_type_of_contract():
+    contract_test = TypeContract(
+        description="Type test",
+        durationLunch=time(1, 0)
+    )
+    contract_test.save()
+    return contract_test
+
+def add_new_report(employee, typeOfContract):
+    report_test = Report(
+        initialDate=date(2020, 1, 1),
+        finalDate=date(2020, 1, 31),
+        employee=employee,
+        typeContract=typeOfContract
+    )
+    report_test.save()
+    return report_test
+
+
+class ReportTestsWithoutData(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_get_all_reports_with_no_reports(self):
+        response = self.client.get('/api/report/')
+        self.assertEqual(response.status_code, 204)
+
+
+class ReportTestsWithData(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.employee_test = add_new_employee()
+        self.contract_test = add_new_type_of_contract()
+        self.report_test = add_new_report(self.employee_test, self.contract_test)
+    
+    def test_get_all_reports(self):
+        response = self.client.get('/api/report/')
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(response_body_data['data'][0]['initialDate'], '2020-01-01')
+    
+    def test_add_new_report(self):
+        json_data = {
+            'initialDate': date(2020, 1, 1),
+            'finalDate': date(2020, 1, 31),
+            'employee_id': self.employee_test.id,
+            'typeContract_id': self.contract_test.id
+        }
+        response = self.client.post(
+            '/api/report/', json_data, headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response_body_data['data']['initialDate'], '2020-01-01')
+    
+    def test_add_new_report_with_wrong_employee(self):
+        json_data = {
+            'initialDate': date(2020, 1, 1),
+            'finalDate': date(2020, 1, 31),
+            'employee_id': self.employee_test.id + 1,
+            'typeContract_id': self.contract_test.id
+        }
+        response = self.client.post(
+            '/api/report/', json_data, headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response_body_data['message'], 'this employee does not exists')
+    
+    def test_add_new_report_with_wrong_type_of_contract(self):
+        json_data = {
+            'initialDate': date(2020, 1, 1),
+            'finalDate': date(2020, 1, 31),
+            'employee_id': self.employee_test.id,
+            'typeContract_id': self.contract_test.id + 1
+        }
+        response = self.client.post(
+            '/api/report/', json_data, headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response_body_data['message'], 'this type of contract does not exists')
+    
+    def test_delete_report(self):
+        response = self.client.delete(
+            f'/api/report/{self.report_test.id}/', headers={"Content-Type": "application/json"})
+        self.assertEqual(response.status_code, 204)
+    
+    def test_delete_no_existent_report(self):
+        response = self.client.delete(
+            f'/api/report/{self.report_test.id + 1}/', headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response_body_data['message'], 'no report founded')
+    
+    def test_retrieve_report(self):
+        response = self.client.get(
+            f'/api/report/{self.report_test.id}/', headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response_body_data['data']['initialDate'], '2020-01-01')
+    
+    def test_retrieve_no_existent_report(self):
+        response = self.client.get(
+            f'/api/report/{self.report_test.id + 1}/', headers={"Content-Type": "application/json"})
+        response_body_data = json.loads(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response_body_data['message'], 'no report founded')
