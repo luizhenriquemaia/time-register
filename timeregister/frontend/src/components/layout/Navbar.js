3258ff1b19586bd13b3510abb0d5aadf540513fa
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-08-04 18:16:07 -0300

    private navlinks

--- timeregister/frontend/src/components/layout/Navbar.js
+++ timeregister/frontend/src/components/layout/Navbar.js
@@ -1,18 +1,24 @@
-// react things 
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 
 export default function Navbar() {
-    return (
-        <nav className="navbar-horizontal">
-            <ul className="list-navbar">
-                <li><Link to="/" className="nav-link">Home</Link></li>
-            </ul>
-            <ul className="list-navbar">
-                <li><Link to="/report" className="nav-link">Reports</Link></li>
-                <li><Link to="/logout" className="nav-link">Sign Out</Link></li>
-            </ul>
-        </nav>
-    )
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+    if (isAuthenticated) {
+        return (
+            <nav className="navbar-horizontal">
+                <ul className="list-navbar">
+                    <li><Link to="/" className="nav-link">Home</Link></li>
+                </ul>
+                <ul className="list-navbar">
+                    <li><Link to="/report" className="nav-link">Reports</Link></li>
+                    <li><Link to="/logout" className="nav-link">Sign Out</Link></li>
+                </ul>
+            </nav>
+        )
+    } else {
+        return <nav className="navbar-horizontal"></nav>
+    }
 }
